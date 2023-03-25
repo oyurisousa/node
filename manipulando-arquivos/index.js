67c6661751d44a4008200c5e907e5d19685ef006
Carlos yuri <yurisousaenfer@gmail.com>
2023-03-25 08:58:30 -0300

    :step:delatar  e renomear arquivos com node

--- manipulando-arquivos/index.js
+++ manipulando-arquivos/index.js
@@ -1,12 +1,6 @@
 const fs = require('fs')
 
 
-fs.readFile('yuri.txt',(err,data)=>{
-    let str = (data.toString())
-
-    //let newStr = str.split('/')
-    let newStr = str.substring(0,7)
-    
-    console.log(newStr)
-
+fs.rename('yuri.txt','yurirnm',(err)=>{
+    console.log('arquivo foi renomeado com sucessso')
 })
