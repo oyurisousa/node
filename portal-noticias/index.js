6781cecc1d3a6bb6282087b5e2314dc7bdfb48e9
Carlos yuri <yurisousaenfer@gmail.com>
2023-03-31 08:12:31 -0300

    :step: header 100%

---
+++ portal-noticias/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.engine("html",require('ejs').renderFile)
+app.set('view engine','html')
+app.use('/public', express.static(path.join(__dirname,'public')))
+app.set('views',path.join(__dirname,'/pages'))
+
+
+app.get('/',(req,res)=>{
+    
+    if(req.query.busca == null){
+        res.render('home',{})
+    }else{
+        res.send('voce buscou'+req.query.busca)
+    }
+    
+})
+
+app.get('/:slug',(req,res)=>{
+    res.send(req.params.slug)
+})
+app.listen(5000,()=>{
+    console.log('rodando')
+})
