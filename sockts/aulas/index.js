92b9620f9e373392ba861f4f8d4e28e39238d540
Carlos yuri <yurisousaenfer@gmail.com>
2023-05-11 08:21:49 -0300

    chat sockets 3/3

--- sockts/aulas/index.js
+++ sockts/aulas/index.js
@@ -1,8 +1,11 @@
+const { disconnect } = require('process');
+
 const app = require('express')();
 const http = require('http').createServer(app)
 const io = require('socket.io')(http);
 
-var usuarios = {}
+var usuarios = []
+var socketIds = []
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/index.html')
@@ -10,8 +13,30 @@ app.get('/',(req,res)=>{
 
 io.on('connection', (socket) => {
 
+  socket.on('new user',(data)=>{
+    if(usuarios.indexOf(data) != -1){
+      socket.emit('new user', {sucess: false})
+    }else{
+      usuarios.push(data)
+      socketIds.push(socket.id)
+      socket.emit('new user',{sucess:true})
+    }
+  })
   socket.on('chat message',(obj)=>{
-    io.emit('chat message', obj)
+    if(usuarios.indexOf(obj.name) && usuarios.indexOf(obj.nome) == socketIds.indexOf(socket.id)){
+      io.emit('chat message',obj)
+    }else{
+      console.log("Erro: você não tem permissão para executara a acao")
+    }
+  })
+  
+  socket.on('disconnect',()=>{
+    let id = socketIds.indexOf(socket.id)
+    socketIds.splice(id,1)
+    usuarios.splice(id,1)
+    console.log(socketIds)
+    console.log(usuarios)
+    console.log("user disconnec ")
   })
 })
 
